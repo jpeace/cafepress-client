f96b00ab36cc9b180c3a9ae730e815df16004893
Jarrod Peace <peace.jarrod@gmail.com>
2011-08-31 04:10:00 -0500

    Initial commit on cafepress project

---
+++ http_helper.rb
@@ -0,0 +1,29 @@
+require 'uri'
+require 'net/http'
+require 'net/https'
+
+class HttpHelper
+  
+  def initialize(uri)
+    @uri = URI.parse(uri)
+  end
+  
+  def build_http
+    http = Net::HTTP.new(@uri.host, @uri.port)
+    if (@uri.port == 443)
+      http.use_ssl = true
+    end
+    return http
+  end
+  
+  def build_get_request
+    return Net::HTTP::Get.new(@uri.request_uri)
+  end
+  
+  def build_post_request(form)
+    request = Net::HTTP::Post.new(@uri.request_uri)
+    request.set_form_data(form) unless form.nil?
+    return request
+  end
+  
+end
